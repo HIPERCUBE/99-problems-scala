0106cb50f415cfddef00f84038823d0fd81e41ae
Joowon <joowonryoo@gmail.com>
2019-01-31 16:53:01 +0900

    Problem 05 solution & test cases

---
+++ src/main/scala/ninetynine/problems/scala/P05.scala
@@ -0,0 +1,13 @@
+package ninetynine.problems.scala
+
+trait P05[T] {
+  def reverse(list: List[T]): List[T]
+}
+
+class P05Impl[T] extends P05[T] {
+  final override def reverse(list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case head :: Nil => List(head)
+    case e1 :: e2 :: tail => reverse(tail) ++ List(e2, e1)
+  }
+}
