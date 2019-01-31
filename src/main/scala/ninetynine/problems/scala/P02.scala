57fc46c461122ec41034d1910ac26907af3a8667
Joowon <joowonryoo@gmail.com>
2019-01-31 15:19:48 +0900

    Update P02: Add tailrec annotation for compiler optimization

--- src/main/scala/ninetynine/problems/scala/P02.scala
+++ src/main/scala/ninetynine/problems/scala/P02.scala
@@ -1,11 +1,14 @@
 package ninetynine.problems.scala
 
+import scala.annotation.tailrec
+
 trait P02[T] {
   def penultimate(list: List[T]): Option[T]
 }
 
 class P02Impl[T] extends P02[T] {
-  override def penultimate(list: List[T]): Option[T] = list match {
+  @tailrec
+  final override def penultimate(list: List[T]): Option[T] = list match {
     case Nil => None
     case _ :: Nil => None
     case penultimate :: _ :: Nil => Some(penultimate)
