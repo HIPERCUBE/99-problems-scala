70a7a2d52338c01f8eef3fe54382e5a87a114034
Joowon <joowonryoo@gmail.com>
2019-02-01 17:47:54 +0900

    Problem 13 solution & test cases

---
+++ src/test/scala/ninetynine/problems/scala/P13Spec.scala
@@ -0,0 +1,13 @@
+package ninetynine.problems.scala
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class P13Spec extends FlatSpec with Matchers {
+  val p13: P13[Char] = new P13Impl[Char]
+
+  "A list with duplicated elements" should "returned with encoded element" in {
+    val list = List('a', 'a', 'b', 'c', 'c', 'c')
+    val expected = List((2, 'a'), (1, 'b'), (3, 'c'))
+    p13.encodeDirect(list) should equal(expected)
+  }
+}
