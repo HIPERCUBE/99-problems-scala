ebb11bbf7caa710f549340e557ddb5b4b13f2d0a
Joowon <joowonryoo@gmail.com>
2019-02-01 18:07:40 +0900

    Problem 15 solution & test cases

---
+++ src/main/scala/ninetynine/problems/scala/P15.scala
@@ -0,0 +1,16 @@
+package ninetynine.problems.scala
+
+trait P15[T] {
+  def duplicateN(n: Int, list: List[T]): List[T]
+
+  def duplicateNR(n: Int, list: List[T]): List[T]
+}
+
+class P15Impl[T] extends P15[T] {
+  final override def duplicateN(n: Int, list: List[T]): List[T] = list.flatMap(e => List.fill(n)(e))
+
+  final override def duplicateNR(n: Int, list: List[T]): List[T] = list match {
+    case e :: tail => List.fill(n)(e) ++ duplicateNR(n, tail)
+    case Nil => Nil
+  }
+}
