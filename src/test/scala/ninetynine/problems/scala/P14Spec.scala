9c59822fc55b32940bcc5084740eb40d42545a60
Joowon <joowonryoo@gmail.com>
2019-02-01 17:59:02 +0900

    Problem 14 solution & test cases

---
+++ src/test/scala/ninetynine/problems/scala/P14Spec.scala
@@ -0,0 +1,19 @@
+package ninetynine.problems.scala
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class P14Spec extends FlatSpec with Matchers {
+  val p14: P14[Int] = new P14Impl[Int]
+
+  "Duplicate method" should "work correctly" in {
+    val list = List(1, 2, 3, 3, 4, 5)
+    val expected = List(1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5)
+    p14.duplicate(list) should equal(expected)
+  }
+
+  "DuplicateR method" should "work correctly" in {
+    val list = List(1, 2, 3, 3, 4, 5)
+    val expected = List(1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5)
+    p14.duplicateR(list) should equal(expected)
+  }
+}
