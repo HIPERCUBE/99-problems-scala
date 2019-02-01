1534f072be8defd096f1100a299c8049e745f10e
Joowon <joowonryoo@gmail.com>
2019-02-01 13:31:04 +0900

    Problem 09 solution & test cases

---
+++ src/main/scala/ninetynine/problems/scala/P09.scala
@@ -0,0 +1,18 @@
+package ninetynine.problems.scala
+
+trait P09[T] {
+  def pack(list: List[T]): List[List[T]]
+}
+
+class P09Impl[T] extends P09[T] {
+  override def pack(list: List[T]): List[List[T]] = {
+    def packInner(acc: List[T], lt: List[T]): List[List[T]] = lt match {
+      case e :: tail if acc.isEmpty => packInner(List(e), tail)
+      case e :: tail if acc.head == e => packInner(e :: acc, tail)
+      case e :: tail => acc :: packInner(List(e), tail)
+      case Nil => List(acc)
+    }
+
+    packInner(List(), list)
+  }
+}
---
+++ src/test/scala/ninetynine/problems/scala/P09Spec.scala
@@ -0,0 +1,17 @@
+package ninetynine.problems.scala
+
+import org.scalatest.{FlatSpec, FunSuite, Matchers}
+
+class P09Spec extends FlatSpec with Matchers {
+  val p09: P09[Int] = new P09Impl[Int]
+
+  "A list without duplicates" should "returned with elements packed within one-element lists" in {
+    p09.pack((0 to 3).toList) should equal(List(List(0), List(1), List(2), List(3)))
+  }
+
+  it should "pack consecutive duplicates" in {
+    val duplicates = List(1, 1, 1, 2, 2, 2, 2, 3, 4, 4, 4, 4)
+    val packed = List(List(1, 1, 1), List(2, 2, 2, 2), List(3), List(4, 4, 4, 4))
+    p09.pack(duplicates) should equal(packed)
+  }
+}
