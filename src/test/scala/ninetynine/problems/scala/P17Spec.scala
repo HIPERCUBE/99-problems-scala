a1b639e74a35fe334cd185d54cd2d50912ea01c4
HIPERCUBE <joowonryoo@gmail.com>
2019-02-04 23:03:19 +0900

    Problem 18 solution & test case

--- src/test/scala/ninetynine/problems/scala/P17Spec.scala
+++ src/test/scala/ninetynine/problems/scala/P17Spec.scala
@@ -10,15 +10,15 @@ class P17Spec extends FlatSpec with Matchers {
 
   it should "split a empty list into one part" in {
     p17s.foreach(p17 => {
-      p17.split(1, List(1)) should equal(List(List(1)))
-      p17.split(2, List(1, 2)) should equal(List(List(1, 2)))
+      p17.split(1, List(1)) should equal((List(1), List()))
+      p17.split(2, List(1, 2)) should equal((List(1, 2), List()))
     })
   }
 
   it should "split a list into two part" in {
     p17s.foreach(p17 => {
-      p17.split(1, (1 to 5).toList) should equal(List(List(1), (2 to 4).toList))
-      p17.split(3, (1 to 10).toList) should equal(List((1 to 3).toList, (4 to 10).toList))
+      p17.split(1, (1 to 5).toList) should equal((List(1), (2 to 5).toList))
+      p17.split(3, (1 to 10).toList) should equal(((1 to 3).toList, (4 to 10).toList))
     })
   }
 }
