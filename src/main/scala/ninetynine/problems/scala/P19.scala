fd25522d664680135d0be3433e5594a579a87610
HIPERCUBE <joowonryoo@gmail.com>
2019-02-04 23:17:14 +0900

    Problem 19 solution & test case

---
+++ src/main/scala/ninetynine/problems/scala/P19.scala
@@ -0,0 +1,16 @@
+package ninetynine.problems.scala
+
+trait P19[T] {
+  def rotate(n: Int, list: List[T]): List[T]
+}
+
+class P19TakeDrop[T] extends P19[T] {
+  final override def rotate(n: Int, list: List[T]): List[T] = list.drop(n) ++ list.take(n)
+}
+
+class P19RecursiveImpl[T] extends P19[T] {
+  final override def rotate(n: Int, list: List[T]): List[T] = list match {
+    case e :: tail if n > 0 => rotate(n - 1, tail :+ e)
+    case _ => list
+  }
+}
