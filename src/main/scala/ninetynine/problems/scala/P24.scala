892e7111aaf03d2b64a89d94c8154df971ef2d88
HIPERCUBE <joowonryoo@gmail.com>
2019-02-10 16:52:57 +0900

    Problem 24 solution & test case

---
+++ src/main/scala/ninetynine/problems/scala/P24.scala
@@ -0,0 +1,9 @@
+package ninetynine.problems.scala
+
+trait P24 {
+  def lotto(n: Int, range: Int): List[Int]
+}
+
+class P24Impl extends P24 {
+  override def lotto(n: Int, range: Int): List[Int] = new P23RecursiveImpl[Int].randomSelect(n, 1 to range toList)
+}
