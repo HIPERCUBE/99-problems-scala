3f915d7864ed0d7df7674a3009a706a413a7dc6d
HIPERCUBE <joowonryoo@gmail.com>
2019-02-10 17:50:13 +0900

    Problem 25 solution & test case

---
+++ src/test/scala/ninetynine/problems/scala/P25Spec.scala
@@ -0,0 +1,13 @@
+package ninetynine.problems.scala
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class P25Spec extends FlatSpec with Matchers {
+  val p25 = new P25Impl[Int]
+
+  it should "generate a random permutation of the elements of a list" in {
+    val permutated = p25.randomPermute(1 to 10 toList)
+    permutated should not equal (1 to 10 toList)
+    permutated.sorted should equal(1 to 10 toList)
+  }
+}
